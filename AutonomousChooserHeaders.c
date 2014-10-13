f205f3937527ca1519fcde07ee56619a1ea468c3
Kiduke <jkiduke@yahoo.com>
2014-10-13 16:40:55 -0500

    Added file

---
+++ AutonomousChooserHeaders.c
@@ -0,0 +1,24 @@
+static const int mid = 3;
+static const int left = 2;
+static const int right = 1;
+
+static const int Red = 0;
+static const int Blue = 1;
+
+static const int Ramp = 0;
+static const int ParkingZone = 1;
+
+static const int  Offense = 0;
+static const int  Defense = 1;
+
+//Define varaibles
+int Color = Red;
+int Side = Ramp;
+int Strategy = Offense;
+
+void ChooseStrategy();
+
+task main()
+{
+
+}
