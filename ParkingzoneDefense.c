3e90768b291ae3536b915dbf5857d171a5f2747d
Kiduke <jkiduke@yahoo.com>
2014-10-16 15:58:21 -0500

    Deleted defense code

--- ParkingzoneDefense.c
+++ ParkingzoneDefense.c
@@ -5,33 +5,6 @@
 */
 task main()
 {
-initializeRobot();
-
-  waitForStart(); // Wait for the beginning of autonomous phase.
-
-	//move forward
-	motor[motorTL] = 60;
-	motor[motorTR] = 60;
-	motor[motorBL] = 60;
-	motor[motorBR] = 60;
-
-	//move back and forth
-	while(true)
-	{
-		motor[motorTL] = 60;
-		motor[motorTR] = 60;
-		motor[motorBL] = 60;
-		motor[motorBR] = 60;
-
-		wait1Msec(1000);
-
-		motor[motorTL] = -60;
-		motor[motorTR] = -60;
-		motor[motorBL] = -60;
-		motor[motorBR] = -60;
-
-		wait1Msec(1000);
-	}
 
 
 }
