e9d19cf260421ebbdcb963b5a5da58e3b9522395
Kiduke <jkiduke@yahoo.com>
2014-10-20 17:31:20 -0500

    fixed methods, now work

--- ParkingzoneOne.c
+++ ParkingzoneOne.c
@@ -8,5 +8,17 @@
 
 task main()
 {
+	//reset encoders
+	nMotorEncoder[motorTR] = 0;
+	nMotorEncoder[motorTL] = 0;
+	nMotorEncoder[motorBR] = 0;
+	nMotorEncoder[motorBL] = 0;
 
+	while(nMotorEncoder[motorTR] < 2300)
+	{
+		motor[motorTR] = 20;
+		motor[motorTL] = 20;
+		motor[motorBR] = 20;
+		motor[motorBL] = 20;
+	}
 }
